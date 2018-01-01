f31045a34e9096e2bd5501c8e9a81b68886ed7e2
mauro tubiana <tubix@de-think>
2018-01-01 23:24:26 +0100

    add struct to increase customization in snake seq

--- sw/arduino/albero/snake.h
+++ sw/arduino/albero/snake.h
@@ -2,7 +2,12 @@
 #define SNAKE_SEQ_H
 #include "color_seq.h"
 
+struct snake_config {
+	uint8_t reset_colors;
+};
+
 void snake_init(void);
+void snake_set_config(struct snake_config config);
 void snake_update(struct color_seq *col, void *data);
 void snake_set_color(rgb_color color);
 // int stars_seq_set_params(const struct stars_seq_params *params);
